126587fd8f9dc0dfd59d5a45694e1d5aa1e68e1e
Oracle Public Cloud User <opc@antman.subnet06281959.vcn06281959.oraclevcn.com>
2022-07-03 19:01:00 +0900

    2022-07-03

--- src/main/java/com/example/AntMan/repository/JdbcTestRepository.java
+++ src/main/java/com/example/AntMan/repository/JdbcTestRepository.java
@@ -1,5 +1,41 @@
-package com.example.AntMan.repository;
+// package com.example.AntMan.repository;
 
-public class JdbcTestRepository implements MemberRepository {
+// public class JdbcTestRepository implements TestRepository {
+
+//     private final DataSource data;
+
+//     public JdbcTestRepository(DataSource data) {
+//         this.data = data;
+//     }
+
+//     @Override
+//     public Test save(Test test) {
+//         String sql = "insert into test(id,name) values(?,?)";
+
+//         Connection conn = data.getConnection();
+
+//         PreparedStatement pstmt = conn.preparedStatement(sql);
+//         pstmt.setString(1, test.getNo());
+//         pstmt.setString(2, test.getName());
+
+//         pstmt.executeUpdate();
+
+//         return null;
+//     }
+
+//     @Override
+//     public Test save(Test test) {
+        
+//     }
+
+//     @Override
+//     public Test save(Test test) {
+        
+//     }
+
+//     @Override
+//     public Test save(Test test) {
+        
+//     }
     
-}
+// }
