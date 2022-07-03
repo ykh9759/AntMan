ebd677c6da1a2841f05c4d39b1762c15fb5a3efb
Oracle Public Cloud User <opc@antman.subnet06281959.vcn06281959.oraclevcn.com>
2022-07-03 19:01:45 +0900

    2022-07-03

---
+++ src/main/java/com/example/AntMan/repository/TestRepository.java
@@ -0,0 +1,9 @@
+// import java.util.List;
+// import java.util.Optional;
+
+// public interface TestRepository {
+//     Test save(Test test);
+//     Optional<Test> findById(Int no);
+//     Optional<Test> findByName(String name);
+//     List<Test> findAll();
+// }
