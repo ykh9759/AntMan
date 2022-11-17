470e01a6b4a01be2637608b339b1286415752a60
hoon <ykh059@naver.com>
2022-11-17 21:16:40 +0900

    2022-11-17

--- src/main/java/com/example/AntMan/controller/CommunityController.java
+++ src/main/java/com/example/AntMan/controller/CommunityController.java
@@ -31,29 +31,29 @@ import com.example.AntMan.domain.entity.Reply;
 import com.example.AntMan.service.BoardService;
 import com.example.AntMan.utils.Alert;
 
-
 @Controller
 public class CommunityController {
-	
-	@Autowired
+
+    @Autowired
     BoardService boardService;
 
-	Board board;
-	
-	Reply reply;
-	
-	@RequestMapping(value = "/community/detail/{id}")
-	public String detail(HttpServletRequest request, HttpServletResponse response, Model model, @PathVariable("id") Integer id) {
-		// 게시물 디테일
-		BoardDetail boardDetail = this.boardService.getBoardDetail(id);
-		model.addAttribute("boardDetail", boardDetail);
-		// 게시물 아이디의 댓글
-		List<ReplyList> replyList = this.boardService.getReplyList(id);
-		model.addAttribute("replyList", replyList);		
-		// 게시물 아이디
-		model.addAttribute("boardNo", id);
-		
-		HttpSession session = request.getSession(false);
+    Board board;
+
+    Reply reply;
+
+    @RequestMapping(value = "/community/detail/{id}")
+    public String detail(HttpServletRequest request, HttpServletResponse response, Model model,
+            @PathVariable("id") Integer id) {
+        // 게시물 디테일
+        BoardDetail boardDetail = this.boardService.getBoardDetail(id);
+        model.addAttribute("boardDetail", boardDetail);
+        // 게시물 아이디의 댓글
+        List<ReplyList> replyList = this.boardService.getReplyList(id);
+        model.addAttribute("replyList", replyList);
+        // 게시물 아이디
+        model.addAttribute("boardNo", id);
+
+        HttpSession session = request.getSession(false);
         Member member = null;
 
         if (session != null) {
@@ -63,36 +63,36 @@ public class CommunityController {
             return "";
         }
         model.addAttribute("member", member);
-		
-		
-		return "community/community_detail";
-	}
-	
-	@GetMapping("/community")
-    public String community(HttpServletRequest request, HttpServletResponse response, Model model, @RequestParam String id) {
-		List<EditList> editList = this.boardService.getList(Integer.valueOf(id));
-		List<BoardList> boardList = this.boardService.getDivList();
-		BoardDivList boardName = this.boardService.getDivName(Integer.valueOf(id));
-		model.addAttribute("boardName", boardName);
+
+        return "community/community_detail";
+    }
+
+    @GetMapping("/community")
+    public String community(HttpServletRequest request, HttpServletResponse response, Model model,
+            @RequestParam String id) {
+        List<EditList> editList = this.boardService.getList(Integer.valueOf(id));
+        List<BoardList> boardList = this.boardService.getDivList();
+        BoardDivList boardName = this.boardService.getDivName(Integer.valueOf(id));
+        model.addAttribute("boardName", boardName);
         model.addAttribute("editList", editList);
         model.addAttribute("boardList", boardList);
-        
+
         HttpSession session = request.getSession(false);
         Member member = null;
 
         if (session != null) {
             member = (Member) session.getAttribute("LOGIN_MEMBER");
         }
-        
+
         model.addAttribute("member", member);
-        
-		return "community/board";
+
+        return "community/board";
     }
-	
-	@GetMapping("/board-edit")
+
+    @GetMapping("/board-edit")
     public String boardEdit(HttpServletRequest request, HttpServletResponse response, Model model) {
-		
-		HttpSession session = request.getSession(false);
+
+        HttpSession session = request.getSession(false);
         Member member = null;
 
         if (session != null) {
@@ -102,53 +102,54 @@ public class CommunityController {
             return "";
         }
         model.addAttribute("member", member);
-		
+
         return "community/boardEdit";
-    }	
-	
-	@PostMapping("/board-edit/save")
-	public void EditCreate(@Valid Edit edit, Errors errors, HttpServletResponse response) throws Exception {
-        
-		if (errors.hasErrors()) {
+    }
+
+    @PostMapping("/board-edit/save")
+    public void EditCreate(@Valid Edit edit, Errors errors, HttpServletResponse response) throws Exception {
+
+        if (errors.hasErrors()) {
             Map<String, String> validatorResult = boardService.validateHandling(errors);
 
             for (String key : validatorResult.keySet()) {
-            	Alert.alertAndBackPage(response, validatorResult.get(key));
+                Alert.alertAndBackPage(response, validatorResult.get(key));
                 return;
             }
         }
-		
-		board = edit.toEntity();
+
+        board = edit.toEntity();
 
         boardService.boardSave(board);
 
         Alert.alertAndMovePage(response, "등록 되었습니다.", "/community?id=1");
-		
-		return;
+
+        return;
     }
-	
-	// 댓글 SAVE
-	@PostMapping("/community/reply/save/{id}")
-	public void replyCreate(@Valid ReplySave replySave, @PathVariable("id") Integer id, Errors errors, HttpServletResponse response) throws Exception {
-        
-		if (errors.hasErrors()) {
+
+    // 댓글 SAVE
+    @PostMapping("/community/reply/save/{id}")
+    public void replyCreate(@Valid ReplySave replySave, @PathVariable("id") Integer id, Errors errors,
+            HttpServletResponse response) throws Exception {
+
+        if (errors.hasErrors()) {
             Map<String, String> validatorResult = boardService.validateHandling(errors);
 
             for (String key : validatorResult.keySet()) {
-            	Alert.alertAndBackPage(response, validatorResult.get(key));
+                Alert.alertAndBackPage(response, validatorResult.get(key));
                 return;
             }
         }
-		
-		replySave.setBoardNo(id);
-		
-		reply = replySave.toEntity();
+
+        replySave.setBoardNo(id);
+
+        reply = replySave.toEntity();
 
         boardService.replySave(reply);
 
-        Alert.alertAndMovePage(response, "등록 되었습니다.", "/community/detail/"+id.toString());
-		
-		return;
+        Alert.alertAndMovePage(response, "등록 되었습니다.", "/community/detail/" + id.toString());
+
+        return;
     }
-	
+
 }
--- src/main/java/com/example/AntMan/domain/dto/Edit.java
+++ src/main/java/com/example/AntMan/domain/dto/Edit.java
@@ -12,26 +12,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Edit {
-	
-	//@NotBlank(message = "게시판을 선택해주세요.") <- integer NotBlank 쓰면 오류나는듯
+
+	// @NotBlank(message = "게시판을 선택해주세요.") <- integer NotBlank 쓰면 오류나는듯
 	private Integer boardDiv; // 게시판 분류번호
-	
+
 	// private Integer user_no;
-	
+
 	@NotBlank(message = "제목을 입력해 주세요.")
 	private String title;
-	
+
 	@NotBlank(message = "내용을 입력해 주세요.")
 	private String contents;
-	
+
 	public Board toEntity() {
 		return Board.builder()
-                .boardDiv(boardDiv)
-                .userNo(0) // test
-                .title(title)
-                .contents(contents)
-                .viewCount(0)
-                .build();
+				.boardDiv(boardDiv)
+				.userNo(0) // test
+				.title(title)
+				.contents(contents)
+				.viewCount(0)
+				.build();
 	}
-	
+
 }
